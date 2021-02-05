38eeb4b5b8499a4c4ab907bb445dddbb3245eeb4
Pandapan <1009303269@qq.com>
2021-02-05 10:11:30 +0800

    新增数据库设置db.py

--- buaaac/settings.py
+++ buaaac/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import datetime
 import os
-
+from utils import db
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -92,9 +92,9 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql',
-        'USER': 'root',
-        'PASSWORD': 'zhong123',
+        'NAME': 'buaaac',
+        'USER': db.USER,
+        'PASSWORD': db.PASSWORD,
         'HOST': '127.0.0.1',
     }
 }
