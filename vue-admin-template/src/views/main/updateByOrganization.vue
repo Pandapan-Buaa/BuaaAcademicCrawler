b745630d6f6e8392b7f2434e4502d701740b819b
Pandapan <1009303269@qq.com>
2021-04-10 10:17:05 +0800

    save

--- vue-admin-template/src/views/main/updateByOrganization.vue
+++ vue-admin-template/src/views/main/updateByOrganization.vue
@@ -507,12 +507,7 @@ export default {
   margin-right: auto;
   margin: auto;
 }
-body {
-  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-  overflow: auto;
-  font-weight: 400;
-  -webkit-font-smoothing: antialiased;
-}
+
 .tb-edit .el-input {
   display: none
 }
